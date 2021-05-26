00087acd43dc3a6af3ddeb2166adccd510529b6e
TwentyFive <TwentyFiveSoftware@gmail.com>
2021-05-26 14:35:11 +0200

    add option: long press to move map

--- lib/widgets/MusicMap.dart
+++ lib/widgets/MusicMap.dart
@@ -4,6 +4,8 @@ import '../models/EdgeInfo.dart';
 import './Node.dart';
 import './Edge.dart';
 
+const bool LONG_PRESS_TO_MOVE_MAP = true;
+
 class MusicMap extends StatefulWidget {
   final List<NodeInfo> nodes;
   final List<EdgeInfo> edges;
@@ -16,12 +18,30 @@ class MusicMap extends StatefulWidget {
 
 class _MusicMapState extends State<MusicMap> {
   Offset offset = Offset.zero;
+  Offset moveOffset = Offset.zero;
 
   @override
   Widget build(BuildContext context) {
     return Stack(children: [
       GestureDetector(
+        onLongPressStart: (_) => moveOffset = Offset.zero,
+        onLongPressMoveUpdate: (details) {
+          if (!LONG_PRESS_TO_MOVE_MAP) return;
+          setState(() {
+            moveOffset = details.localOffsetFromOrigin;
+          });
+        },
+        onLongPressEnd: (_) {
+          if (!LONG_PRESS_TO_MOVE_MAP) return;
+          setState(() {
+            offset =
+                Offset(offset.dx + moveOffset.dx, offset.dy + moveOffset.dy);
+            moveOffset = Offset.zero;
+          });
+        },
         onPanUpdate: (details) => setState(() {
+          if (LONG_PRESS_TO_MOVE_MAP) return;
+
           offset = Offset(
             offset.dx + details.delta.dx,
             offset.dy + details.delta.dy,
@@ -38,7 +58,9 @@ class _MusicMapState extends State<MusicMap> {
               widget.nodes.firstWhere((n) => n.id == edge.fromNodeId).key,
               widget.nodes.firstWhere((n) => n.id == edge.toNodeId).key))
           .toList(),
-      ...widget.nodes.map((node) => Node(offset, node, () => setState(() {}))).toList(),
+      ...widget.nodes
+          .map((node) => Node(offset + moveOffset, node, () => setState(() {})))
+          .toList(),
     ]);
   }
 }
